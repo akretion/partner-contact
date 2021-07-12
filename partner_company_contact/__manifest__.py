b6504e58eca5a61cfb885fdbbd30aaaf220f3369
Florian da Costa <florian.dacosta@akretion.com>
2021-07-12 14:27:30 +0200

    Add module partner_company_contact

---
+++ partner_company_contact/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Company Contact",
+    "summary": "Manage company on contact/address if different from main partner",
+    "version": "14.0.1.0.1",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base_setup"],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
